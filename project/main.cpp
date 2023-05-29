55ba3e847353344167da2c147165d9fae8dbcaaa
Ryan Lee <ryanbylee@ucla.edu>
2023-05-29 01:30:33 -0700

    establish connection with/receive packets from multiple hosts

--- project/main.cpp
+++ project/main.cpp
@@ -11,7 +11,7 @@
 #include <unistd.h>
 
 #define MAXCLIENTS 30
-#define BUFFER_SIZE 1024
+#define BUFFER_SIZE 2048
 #define DEFAULT_PORT 5152
 
 
@@ -30,15 +30,19 @@ int main(int argc, char *argv[]) {
     std::cout << "Server's LAN IP: " << szLanIp << std::endl
               << "Server's WAN IP: " << szWanIp << std::endl;
     
-    std::string IPClients[10];
-    int i = 0;
+    std::string IPClients[MAXCLIENTS];
+    int hostCounter = 0;
+
+    //skip WAN address (0.0.0.0)
+    //std::getline(std::cin, szLine);
+
     while (szLine != ""){
         std::getline(std::cin, szLine);
-        IPClients[i] = szLine;
-        i++;
+        IPClients[hostCounter] = szLine;
+        hostCounter++;
     }
 
-    for (int i = 0; i < 10; i++){
+    for (int i = 0; i < hostCounter; i++){
         std::cerr << IPClients[i] << std::endl;
     }
 
@@ -79,16 +83,38 @@ int main(int argc, char *argv[]) {
 
     addrlen = sizeof(address);
 
-    while (1) {
+    
+
+    for (int i = 0; i < hostCounter - 1; i++){
+        new_socket = accept(listening_socket, (struct sockaddr *)&address, (socklen_t *)&addrlen);
+
+        if (new_socket < 0) {
+            perror("accept");
+            exit(EXIT_FAILURE);
+        }
+
+        printf("Client connected, ip: %s, port: %d\n",
+                inet_ntoa(address.sin_addr), ntohs(address.sin_port));
+        
+        for (int i = 0; i < MAXCLIENTS; i++) {
+            if (client_sockets[i] == 0) {
+                client_sockets[i] = new_socket;
+                std::cout << "client's new ip address: " << IPClients[i] << std::endl;
+                break;
+            }
+        }
+    }
+    
         // Create a "set" of FD to include all FDs of interest
         // Clear the set and include them one by one
+    while (1) {
+        
         FD_ZERO(&readfds);
 
         // Add server listener socket to the set (for demonstration purpose)
         // There is no need to do it for project 2, as you can wait for all
         // connections to complete and receive data later
-        FD_SET(listening_socket, &readfds);
-        max_sd = listening_socket;
+   
 
         // Include *connected* sockets for clients (those are not 0)
         for (int i = 0; i < MAXCLIENTS; i++) {
@@ -107,42 +133,26 @@ int main(int argc, char *argv[]) {
         // Call select function on the set, we are only interested in the read events
         // so we set all others as 0
         // It will return when any of the socket in the set has the event
-        activity = select(max_sd + 1, &readfds, NULL, NULL, NULL);
+       
 
+
+        // Accept the connection and store the socket in the array to receive data
+        // from the client
+        
+        
+
+        activity = select(max_sd + 1, &readfds, NULL, NULL, NULL);
         if ((activity < 0) && (errno != EINTR)) {
             perror("select");
         }
-
-        // (For demo purpose) If there is event on the listening socket, it means that
-        // another client is connecting
-        if (FD_ISSET(listening_socket, &readfds)) {
-            // Accept the connection and store the socket in the array to receive data
-            // from the client
-            new_socket = accept(listening_socket, (struct sockaddr *)&address, (socklen_t *)&addrlen);
-
-            if (new_socket < 0) {
-                perror("accept");
-                exit(EXIT_FAILURE);
-            }
-
-            printf("Client connected, ip: %s, port: %d\n",
-                   inet_ntoa(address.sin_addr), ntohs(address.sin_port));
-
-            for (int i = 0; i < MAXCLIENTS; i++) {
-                if (client_sockets[i] == 0) {
-                    client_sockets[i] = new_socket;
-                    break;
-                }
-            }
-        }
-
         // Check the connection sockets to see if there is incoming data from
         // each client
         for (int i = 0; i < MAXCLIENTS; i++) {
             sd = client_sockets[i];
-
+            //std::cout << "checking socket: " << sd << std::endl;
             if (sd > 0 && FD_ISSET(sd, &readfds)) {
                 // Receive data from client if there is data
+                std::cout << "Receiving data from: " << IPClients[i] << std::endl;
                 valread = read(sd, buffer, BUFFER_SIZE);
                 buffer[valread] = '\0';
 
@@ -151,7 +161,8 @@ int main(int argc, char *argv[]) {
                     client_sockets[i] = 0;
                 } else {
                     printf("Client %d: %s\n", i, buffer);
-                    send(sd, buffer, strlen(buffer), 0);
+                    
+                    //send(sd, buffer, strlen(buffer), 0);
                 }
             }
         }
