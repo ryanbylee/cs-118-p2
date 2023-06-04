92fcb57ead60bce95e78a06c7240c15ee2eec2c4
Ryan Lee <ryanbylee@ucla.edu>
2023-06-03 23:57:56 -0700

    translate WAN-LAN

--- project/main.cpp
+++ project/main.cpp
@@ -14,6 +14,7 @@
 #include <netinet/udp.h>
 #include <vector>
 #include <unordered_map>
+using namespace std;
 
 #define MAXCLIENTS 30
 #define BUFFER_SIZE 2048
@@ -22,8 +23,9 @@
 //information contained in each entry in NAPT table
 struct napt_entry{
     std::string lan_ip;
-    std::string lan_port;
-    std::string wan_port;
+    std::string wan_ip; // will be the same IP for every entry
+    int lan_port;
+    int wan_port;
 };
 
 int main(int argc, char *argv[]) {
@@ -77,8 +79,9 @@ int main(int argc, char *argv[]) {
         }
         
         entry.lan_ip = infoPerLine[0];
-        entry.lan_port = infoPerLine[1];
-        entry.wan_port = infoPerLine[2];
+        entry.wan_ip = szWanIp;
+        entry.lan_port = stoi(infoPerLine[1]);
+        entry.wan_port = stoi(infoPerLine[2]);
 
         napt_table[tableEntryNum] = entry;
         tableEntryNum++;
@@ -86,7 +89,7 @@ int main(int argc, char *argv[]) {
     }
 
     for (int i = 0; i < 2; i++){
-        std::cout << "entry " << i << ": " << napt_table[i].lan_ip << " " << napt_table[i].lan_port << " " << napt_table[i].wan_port << std::endl;
+        std::cout << "entry " << i << ": " << napt_table[i].lan_ip << " " << napt_table[i].wan_ip << " " << napt_table[i].lan_port << " " << napt_table[i].wan_port << std::endl;
     }
 
     int listening_socket, new_socket, sd, max_sd;
@@ -104,6 +107,8 @@ int main(int argc, char *argv[]) {
     address.sin_family = AF_INET;
     address.sin_addr.s_addr = INADDR_ANY;
     address.sin_port = htons(DEFAULT_PORT);
+
+    // ADDED CODE ELIMINATES BIND: ADDR ALREADY IN USE ERROR; GIVES RUN-TIME ERRORS IN GRADESCOPE
     const int yes = 1;
     if (setsockopt(listening_socket, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) < 0){
         perror("server: setsockopt");
@@ -171,6 +176,12 @@ int main(int argc, char *argv[]) {
                 // Receive data from client if there is data
                 std::cout << "Receiving data from: " << c.first << std::endl;
                 valread = recv(sd, buffer, BUFFER_SIZE, 0);
+                if (valread == 0){
+                    std::cout << "valread is zero" << std::endl;
+                    close(sd);
+                    client_sockets[c.first] = 0;
+                    break;
+                }
                 std::cout << "read: " << valread << " bytes" << std::endl;
                 buffer[valread] = '\0';
                 printf("data: %s\n", buffer);   
@@ -185,10 +196,152 @@ int main(int argc, char *argv[]) {
                 std::cout << "headerLen: " << hdrLen << std::endl;
                 
                 auto ttl = static_cast<size_t>(incomingIpHdr->ttl);
-                std::cout << "ttl: " << ttl << std::endl;
-                incomingIpHdr->ttl -= 1;
+                std::cout << "ttl (before): " << ttl << std::endl;
+                if (ttl <= 1) {
+                    // TODO: drop
+                }
+                else {
+                    incomingIpHdr->ttl -= 1;
+                }
+                ttl = static_cast<size_t>(incomingIpHdr->ttl);
+                cout << "ttl (after)" << ttl << endl;
 
                 std::cout << "checksum (before): " << incomingIpHdr->check << std::endl;
+                
+
+                // source and destination IP addresses
+                struct in_addr source, destination;
+                source.s_addr = incomingIpHdr->saddr;
+                destination.s_addr = incomingIpHdr->daddr;                                
+                std::cout << "source address: " << inet_ntoa(source) << " destination address: " << inet_ntoa(destination) << std::endl;
+
+// my notes: incomingIpHdr --> addr
+//           incomingTCPHdr or incomingUDPHdr --> port #
+// struct format: lanip, wanip, lanPort, wanPort
+// if lan to lan don't need to translate anything
+
+
+                
+
+                //
+                //from LAN TO WAN-swith sourceaddr to server WAN addr
+                // if (inet_ntoa(destination) == "10.0.0.10"){
+                //     for (int i = 0; i < tableEntryNum; i++){
+                //         if (napt_table[i].lan_ip == inet_ntoa(source)){
+                //             incomingIpHdr->saddr = inet_aton(napt_table[i].wan_ip);
+                            
+                //         }
+                //     }
+                    
+                // }
+
+
+                // TCP or UDP protocol
+                if (incomingIpHdr->protocol == IPPROTO_TCP) {
+                    std::cout << "This is a TCP packet" << std::endl;
+                    auto incomingTcpHdr = reinterpret_cast<tcphdr*>(pkt.data() + hdrLen);
+////////////////////////////////////////////////////////////////////////////////////////////////////
+                    // NAPT magic
+                        // inet_ntoa(source) != "10.0.0.10" && inet_ntoa(destination) != "10.0.0.10"
+                    if (inet_ntoa(source) == szLanIp && inet_ntoa(destination) == szLanIp) {
+                        // do nothing
+                    }
+                    else {
+                        for (int i = 0; i < tableEntryNum; i++) {
+                            // sending from LAN; need to change to WAN
+                            if (inet_ntoa(source) == napt_table[i].lan_ip) {
+                                // sanity check
+                                auto oldSourcePort = static_cast<size_t>(ntohs(incomingTcpHdr->th_sport));
+                                // change the addresses
+                                inet_aton(napt_table[i].wan_ip.c_str(), &source); // new addr is stored in source.s_addr
+                                // change the ports
+                                incomingTcpHdr->th_sport = htons(napt_table[i].wan_port);
+                                // check the outputs
+                                
+                                //TODO: FIX 
+                                auto newSourcePort = static_cast<size_t>(ntohs(incomingTcpHdr->th_sport));
+                                std::cout << "translate: previousAddr= " << inet_ntoa(source) << ", currentAddr= " << inet_ntoa(source) << std::endl;
+                                std::cout << "translate: previousPort= " << oldSourcePort << ", currentPort= " << newSourcePort << std::endl;
+                                break;
+                            }
+                            // receiving from WAN; need to change to LAN
+                            else if (inet_ntoa(destination) == napt_table[i].wan_ip) {
+                                // sanity check
+                                auto oldDestPort = static_cast<size_t>(ntohs(incomingTcpHdr->th_dport));
+                                // change the addresses
+                                inet_aton(napt_table[i].lan_ip.c_str(), &destination);  // new addr is stored in destination.s_addr
+                                // change the ports
+                                incomingTcpHdr->th_dport = htons(napt_table[i].lan_port);
+                                // check the outputs
+                                //TODO: FIX
+                                auto newDestPort = static_cast<size_t>(ntohs(incomingTcpHdr->th_dport));
+                                std::cout << "translate: previousAddr= " << inet_ntoa(destination) << ", currentAddr= " << inet_ntoa(destination) << std::endl;
+                                std::cout << "translate: previousPort= " << oldDestPort << ", currentPort= " << newDestPort << std::endl;
+                                break;
+                            }
+                        }
+                    }
+////////////////////////////////////////////////////////////////////////////////////////////////////
+                    auto sourcePort = static_cast<size_t>(ntohs(incomingTcpHdr->th_sport));
+                    auto destPort = static_cast<size_t>(ntohs(incomingTcpHdr->th_dport));
+                    std::cout << "sourePort: "<< sourcePort << " destPort: "<< destPort << std::endl;
+                    auto tcpHdrLen = static_cast<size_t>(incomingTcpHdr->th_off) * 4;
+                    std::cout << "payloadLen: "<< totLen - hdrLen - tcpHdrLen << std::endl;
+
+                    
+                }
+                else if (incomingIpHdr->protocol == IPPROTO_UDP) {
+                    std::cout << "This is a UDP packet" << std::endl;
+                    auto incomingUdpHdr = reinterpret_cast<udphdr*>(pkt.data() + hdrLen);
+////////////////////////////////////////////////////////////////////////////////////////////////////
+                    // NAPT magic
+                    if (inet_ntoa(source) == szLanIp && inet_ntoa(destination) == szLanIp) {
+                        // do nothing
+                    }
+                    else {
+                        for (int i = 0; i < tableEntryNum; i++) {
+                            // sending from LAN; need to change to WAN
+                            if (inet_ntoa(source) == napt_table[i].lan_ip) {
+                                // sanity check
+                                auto oldSourcePort = static_cast<size_t>(ntohs(incomingUdpHdr->uh_sport));
+                                // change the addresses
+                                inet_aton(napt_table[i].wan_ip.c_str(), &source); // new addr is stored in source.s_addr
+                                // change the ports
+                                incomingUdpHdr->uh_sport = htons(napt_table[i].wan_port);
+                                // check the outputs
+                                
+                                //TODO: FIX 
+                                auto newSourcePort = static_cast<size_t>(ntohs(incomingUdpHdr->uh_sport));
+                                std::cout << "translate: previousAddr= " << inet_ntoa(source) << ", currentAddr= " << inet_ntoa(source) << std::endl;
+                                std::cout << "translate: previousPort= " << oldSourcePort << ", currentPort= " << newSourcePort << std::endl;
+                                break;
+                            }
+                            // receiving from WAN; need to change to LAN
+                            else if (inet_ntoa(destination) == napt_table[i].wan_ip) {
+                                // sanity check
+                                auto oldDestPort = static_cast<size_t>(ntohs(incomingUdpHdr->uh_dport));
+                                // change the addresses
+                                inet_aton(napt_table[i].lan_ip.c_str(), &destination);  // new addr is stored in destination.s_addr
+                                // change the ports
+                                incomingUdpHdr->uh_dport = htons(napt_table[i].lan_port);
+                                // check the outputs
+                                //TODO: FIX
+                                auto newDestPort = static_cast<size_t>(ntohs(incomingUdpHdr->uh_dport));
+                                std::cout << "translate: previousAddr= " << inet_ntoa(destination) << ", currentAddr= " << inet_ntoa(destination) << std::endl;
+                                std::cout << "translate: previousPort= " << oldDestPort << ", currentPort= " << newDestPort << std::endl;
+                                break;
+                            }
+                        }
+                    }
+////////////////////////////////////////////////////////////////////////////////////////////////////
+                    auto sourcePort = static_cast<size_t>(ntohs(incomingUdpHdr->uh_sport));
+                    auto destPort = static_cast<size_t>(ntohs(incomingUdpHdr->uh_dport));
+                    std::cout << "sourcePort: "<< sourcePort << " destPort: "<< destPort << std::endl;
+                }
+                
+                
+                std::cout << "source address: " << inet_ntoa(source) << " destination address: " << inet_ntoa(destination) << std::endl;
+
                 // The checksum field is the 16 bit one's complement of the one's
                 // complement sum of all 16 bit words in the header.  For purposes of
                 // computing the checksum, the value of the checksum field is zero.
@@ -207,35 +360,11 @@ int main(int argc, char *argv[]) {
                 incomingIpHdr->check = sum;
                 // incomingIpHdr->check = htons(sum);
                 std::cout << "checksum (after): " << incomingIpHdr->check << std::endl;
-
-                // source and destination IP addresses
-                struct in_addr source, destination;
-                source.s_addr = incomingIpHdr->saddr;
-                destination.s_addr = incomingIpHdr->daddr;                                
-                std::cout << "source address: " << inet_ntoa(source) << " destination address: " << inet_ntoa(destination) << std::endl;
-                
-                // TCP or UDP protocol
-                if (incomingIpHdr->protocol == IPPROTO_TCP) {
-                    std::cout << "This is a TCP packet" << std::endl;
-                    auto incomingTcpHdr = reinterpret_cast<tcphdr*>(pkt.data() + hdrLen);
-                    auto sourcePort = static_cast<size_t>(ntohs(incomingTcpHdr->th_sport));
-                    auto destPort = static_cast<size_t>(ntohs(incomingTcpHdr->th_dport));
-                    std::cout << "sourePort: "<< sourcePort << " destPort: "<< destPort << std::endl;
-                    auto tcpHdrLen = static_cast<size_t>(incomingTcpHdr->th_off) * 4;
-                    std::cout << "payloadLen: "<< totLen - hdrLen - tcpHdrLen << std::endl;
-                }
-                else if (incomingIpHdr->protocol == IPPROTO_UDP) {
-                    std::cout << "This is a UDP packet" << std::endl;
-                    auto incomingUdpHdr = reinterpret_cast<udphdr*>(pkt.data() + hdrLen);
-                    auto sourcePort = static_cast<size_t>(ntohs(incomingUdpHdr->uh_sport));
-                    auto destPort = static_cast<size_t>(ntohs(incomingUdpHdr->uh_dport));
-                    std::cout << "sourcePort: "<< sourcePort << " destPort: "<< destPort << std::endl;
-                }
-                
-                if(send(client_sockets[inet_ntoa(destination)], pkt.data(), pkt.size(), 0) == -1)
+                if(send(client_sockets[inet_ntoa(destination)], incomingIpHdr, pkt.size(), 0) == -1)
                 {
                     perror("send");
-                    close(client_sockets[inet_ntoa(destination)]);
+                    std::cout << "send got -1" << std::endl;
+                    exit(1);
                 }
             }
         }
